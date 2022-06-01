eb125a3b22b931645a2706ee2c8156d92772a998
Nina Kin <matikin9@gmail.com>
2022-05-31 23:38:01 -0700

    index page implementation

--- .eleventy.js
+++ .eleventy.js
@@ -1,9 +1,10 @@
 module.exports = function(eleventyConfig) {
 	eleventyConfig.addPassthroughCopy("src/css");
 	eleventyConfig.addPassthroughCopy("src/js");
+	eleventyConfig.addPassthroughCopy("src/img");
 
 	return {
-		pathPrefix: "/11ty-web-template/",
+		pathPrefix: "/mybus-v2/",
 		dir: {
 			input: "src",
 			output: "docs"
