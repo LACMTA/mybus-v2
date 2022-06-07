45d9af29fc59c7a643bbbe059e738471c1e079c2
Nina Kin <matikin9@gmail.com>
2022-06-06 17:21:10 -0700

    all changes page

--- .eleventy.js
+++ .eleventy.js
@@ -1,13 +1,17 @@
+const isProduction = process.env.NODE_ENV === "prod";
+console.log(process.env.NODE_ENV);
+
 module.exports = function(eleventyConfig) {
 	eleventyConfig.addPassthroughCopy("src/css");
 	eleventyConfig.addPassthroughCopy("src/js");
 	eleventyConfig.addPassthroughCopy("src/img");
+	eleventyConfig.addPassthroughCopy("src/files");
 
 	return {
-		pathPrefix: "/mybus-v2/",
+		pathPrefix: isProduction ? "" : "/mybus-v2/",
 		dir: {
 			input: "src",
 			output: "docs"
 		}
-	}
-}
+	};
+};
