bd63a52bf7ed65b17d3a254e72b8a0f52560e6ea
Nina Kin <matikin9@gmail.com>
2022-06-14 23:13:13 -0700

    refactored rename script for better console logging

--- rename.js
+++ rename.js
@@ -6,21 +6,18 @@ const dir = 'src/files/schedules/';
 const files = readdirSync(dir);
 
 files.forEach(file => {
-	console.log(file);
 	let newFileName = '';
 
 	let match = file.match(/^([0-9]*)( |_|-)*([0-9]*)( |_|-)*(TT)( |_|-)*([0-9]*)( |_|-)*([0-9]*)( |_|-)*([0-9]*)( |_|-)*.pdf$/);
 	
 	if (match[3] == '') {
 		newFileName = `${match[1]}_TT_${match[7]}-${match[9]}-${match[11]}.pdf`;
-		console.log(newFileName);
 	} else {
 		newFileName = `${match[1]}-${match[3]}_TT_${match[7]}-${match[9]}-${match[11]}.pdf`;
-		console.log(newFileName);
 	}
 
-	const filePath = join(dir, file);
-	const newFilePath = join(dir, newFileName);
-
-	renameSync(filePath, newFilePath);
+	if (file != newFileName) {
+		renameSync(join(dir, file), join(dir, newFileName));
+		console.log(`Renamed:\n${file}\nto\n${newFileName}\n`);
+	}
 });
