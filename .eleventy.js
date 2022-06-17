ceb462617851d6d64d52df0d9da99170f637087a
albertkun <albertk@gmx.com>
2022-06-17 15:59:17 -0700

    feat: `check url` plugin added note: never again broken `schedule.pdfs`!!! remove: footer includes in `default.liquid` feat: converted button to `ahref` for all changes

--- .eleventy.js
+++ .eleventy.js
@@ -1,7 +1,12 @@
+const brokenLinksPlugin = require("eleventy-plugin-broken-links");
 const isProduction = process.env.NODE_ENV === "prod";
+
 console.log(process.env.NODE_ENV);
 
 module.exports = function(eleventyConfig) {
+	eleventyConfig.addPlugin(brokenLinksPlugin,{
+		broken: "error",
+	});
 	eleventyConfig.addPassthroughCopy("src/css");
 	eleventyConfig.addPassthroughCopy("src/js");
 	eleventyConfig.addPassthroughCopy("src/img");
